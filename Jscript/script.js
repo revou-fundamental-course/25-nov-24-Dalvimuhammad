9dcd059f448760319f6139430d44c03d2aa6aeef
Dalvimuhammad <muhammaddalvi272@gmail.com>
2024-12-05 02:20:02 +0700

    responsive 480

--- Jscript/script.js
+++ Jscript/script.js
@@ -39,28 +39,24 @@
       if (bmi < 18.5) {
         kategoriBMI.textContent = "Kekurangan berat badan - Pria";
         kategoriBMI.style.color = "#000000FF";
-        kategoriBMI.style.fontSize = "20px"
         penjelBMI.textContent = "Anda berada dalam kategeri berat badan kurang";
         detailBMI.textContent = "Hubungi dokter Anda untuk informasi lebih lanjut tentang diet dan nutrisi yang tepat untuk meningkatkan kesehatan.";
         detailBMI.style.textAlign = "justify"
       } else if (bmi < 24.9) {
         kategoriBMI.textContent = "Normal - Pria";
         kategoriBMI.style.color = "#000000FF";
-        kategoriBMI.style.fontSize = "20px"
         penjelBMI.textContent = "Anda berada dalam kategori Normal";
         detailBMI.textContent = "Bagus!! Jaga berat badan anda tetap normal dengan makanan seimbang dan olahraga fisik.";
         detailBMI.style.textAlign = "justify"
       } else if (bmi < 29.9) {
         kategoriBMI.textContent = "Kelebihan berat badan - Pria";
         kategoriBMI.style.color = "#000000FF";
-        kategoriBMI.style.fontSize = "20px"
         penjelBMI.textContent = "Anda berada dalam kategori berat badan berlebih";
         detailBMI.textContent = "Cara terbaik untuk menurunkan berat badan adalah dengan mengatur asupan kalori dan melakukan aktivitas fisik. Jika Anda termasuk dalam kategori ini, sebaiknya turunkan berat badan Anda ke berat badan normal"
         detailBMI.style.textAlign = "justify"
       } else {
         kategoriBMI.textContent = "Obesitas - Pria";
         kategoriBMI.style.color = "#E00000FF";
-        kategoriBMI.style.fontSize = "20px"
         penjelBMI.textContent = "Anda berada dalam kategori Obesitas"
         detailBMI.textContent = "Yuk, coba turunkan berat badan Anda dan terapkan pola hidup sehat dengan memperhatikan asupan kalori dan olahraga. Segera hubungi dokter untuk pemeriksaan medis agar Anda tahu risiko yang berkaitan dengan berat badan Anda."
         detailBMI.style.textAlign = "justify"
@@ -69,28 +65,24 @@
       if (bmi < 17.5) {
         kategoriBMI.textContent = "Kekurangan berat badan - Wanita";
         kategoriBMI.style.color = "#000000FF";
-        kategoriBMI.style.fontSize = "20px"
         penjelBMI.textContent = "Anda berada dalam kategeri berat badan kurang";
         detailBMI.textContent = "Hubungi dokter Anda untuk informasi lebih lanjut tentang diet dan nutrisi yang tepat untuk meningkatkan kesehatan.";
         detailBMI.style.textAlign = "justify"
       } else if (bmi < 23.9) {
         kategoriBMI.textContent = "Normal - Wanita";
         kategoriBMI.style.color = "#000000FF";
-        kategoriBMI.style.fontSize = "20px"
         penjelBMI.textContent = "Anda berada dalam kategori Normal";
         detailBMI.textContent = "Bagus!! Jaga berat badan anda tetap normal dengan makanan seimbang dan olahraga fisik.";
         detailBMI.style.textAlign = "justify"
       } else if (bmi < 28.9) {
         kategoriBMI.textContent = "Kelebihan berat badan - Wanita";
         kategoriBMI.style.color = "#000000FF";
-        kategoriBMI.style.fontSize = "20px"
         penjelBMI.textContent = "Anda berada dalam kategori berat badan berlebih";
         detailBMI.textContent = "Cara terbaik untuk menurunkan berat badan adalah dengan mengatur asupan kalori dan melakukan aktivitas fisik. Jika Anda termasuk dalam kategori ini, sebaiknya turunkan berat badan Anda ke berat badan normal"
         detailBMI.style.textAlign = "justify"
       } else {
         kategoriBMI.textContent = "Obesitas - Wanita";
         kategoriBMI.style.color = "#BC0000FF";
-        kategoriBMI.style.fontSize = "20px"
         penjelBMI.textContent = "Anda berada dalam kategori Obesitas"
         detailBMI.textContent = "Yuk, coba turunkan berat badan Anda dan terapkan pola hidup sehat dengan memperhatikan asupan kalori dan olahraga. Segera hubungi dokter untuk pemeriksaan medis agar Anda tahu risiko yang berkaitan dengan berat badan Anda."
         detailBMI.style.textAlign = "justify"
